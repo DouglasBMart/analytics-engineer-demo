92ff241b98f1386d5e6a5f8d045629d7b170aa41
Douglas Borges <drexquantum@gmail.com>
2023-06-14 15:00:38 +0000

    [feature] alter column funcionarios

--- models/Silver/erp/stg_erp__funcionarios.sql
+++ models/Silver/erp/stg_erp__funcionarios.sql
@@ -7,7 +7,8 @@ with
     , renomear as (
         select
             cast(employee_id as int)     as id_funcionario,
-            cast((first_name || ' ' || last_name) as string) as funcionario_nome,
+            cast(reports_to  as int)     as id_gerente,
+            cast((first_name || ' ' || last_name) as string) as funcionario,
             cast(birth_date	 as string)  as func_data_nascimento,		
             cast(hire_date	 as date)    as func_data_contratacao, 		
             cast(address	 as string)  as func_endereco,	
@@ -15,8 +16,7 @@ with
             cast(region		 as string)  as func_regiao,
             cast(postal_code as string)  as func_cep,		
             cast(country	 as string)  as func_pais,										
-            cast(notes		 as string)  as func_notas,
-            cast(reports_to  as int)     as id_gerente
+            cast(notes		 as string)  as func_notas
         
         from fonte_funcionarios
     )
