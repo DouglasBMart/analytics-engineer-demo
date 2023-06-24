84cd66dd27f13524d0fdeb7db07863ae0dc5664f
Douglas Borges <drexquantum@gmail.com>
2023-06-24 23:28:58 +0000

    [fix]stg_erp__clientes

--- models/Silver/erp/stg_erp__clientes.sql
+++ models/Silver/erp/stg_erp__clientes.sql
@@ -5,7 +5,7 @@ with
             , cast(contact_name as string) as nome_cliente
             , cast(company_name as string) as empresa_cliente
             , cast(address as string) as endereco_cliente
-            , cast(postal_code as string) as endereco_cliente
+            , cast(postal_code as string) as cep_cliente
             , cast(city as string) as cidade_cliente
             , cast(region as string) as regiao_cliente
             , cast(country as string) as pais_cliente
